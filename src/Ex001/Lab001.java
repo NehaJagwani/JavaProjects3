60167623a3d800593064ad8ed1fee3f6a62f78d2
NehaJagwani <neha.jagwani02@gmail.com>
2024-04-17 12:44:51 +1000

    adding a new line in the same program

--- src/Ex001/Lab001.java
+++ src/Ex001/Lab001.java
@@ -1,7 +1,10 @@
 package Ex001;
 
+import java.sql.SQLOutput;
+
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Adding file in JavaProjects3 repo in GitHub");
+        System.out.println("Neha is an Automation Test Analyst");
     }
 }
